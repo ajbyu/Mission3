190ecbdab38a34d2c41240fad71b97b9dd3cdd96
nbain00 <zink47@gmail.com>
2023-01-23 13:23:56 -0700

    added game board

--- Mission3/Program.cs
+++ Mission3/Program.cs
@@ -6,14 +6,17 @@ namespace Mission3
     {
         static void Main(string[] args)
         {
-            char[][] gameBoard = new char[][]
-            {
-            new char[] { 'x', 'o', 'o' },
-            new char[] { 'x', 'o', 'o' },
-            new char[] { 'x', 'o', 'o' }
-            };
+            Console.WriteLine("Welcome to Tic Tac Toe!"); // Welcome user
 
-            BoardInfo.PrintBoard(gameBoard);
+            char[][] gameBoard = new char[3][]; // Create game board
+
+            gameBoard[0] = new char[] { '1', '2', '3' };
+            gameBoard[1] = new char[] { '4', '5', '6' };
+            gameBoard[2] = new char[] { '7', '8', '9' };
+
+
+            Console.WriteLine("Please enter the number to place the 'X': ");
+
         }
     }
 }
