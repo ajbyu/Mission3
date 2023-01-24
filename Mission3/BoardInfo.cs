6364bbb0bbd9897660ce48d5e1bf02806d7eba28
aj <69333470+TheCircularK@users.noreply.github.com>
2023-01-23 19:19:18 -0700

    Finished board checking functions.

--- Mission3/BoardInfo.cs
+++ Mission3/BoardInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -25,42 +26,223 @@ namespace Mission3
         /// <param name="gameBoard"></param>
         public static void PrintBoard(char[][] gameBoard)
         {
-            Console.WriteLine("Game Board");
-            Console.WriteLine("------------");
+            Console.WriteLine("Game Board\n");
+
+            bool firstRow = true;
             foreach (var line in gameBoard)
             {
-                Console.Write("|");
+                if (!firstRow)
+                {
+                    Console.WriteLine("\n------------");
+                }
+                else
+                {
+                    firstRow = false;
+                }
+                bool firstInLine = true;
                 foreach (var entry in line)
                 {
-                    Console.Write(entry);
-                    Console.Write(" | ");
+                    if (firstInLine)
+                    {
+                        Console.Write(entry);
+                        firstInLine = false;
+                    }
+                    else
+                    {
+                        Console.Write(" | ");
+                        Console.Write(entry);
+                    }
                 }
-                Console.WriteLine("\n------------");
             }
+            Console.WriteLine("\n");
         }
 
+        /// <summary>
+        /// Call this method to check for a winner or a cat's game
+        /// </summary>
+        /// <param name="gameBoard"></param>
+        /// <returns></returns>
         public static GameState CheckWin(char[][] gameBoard)
         {
             GameState state = GameState.NoWinner;
+            int length = gameBoard[0].Length;
 
-            //Check for 
+            //Check lines across
+            state = CheckRows(gameBoard, state);
+
+            //Check vertical lines
+            state = CheckColumns(gameBoard, state);
+
+            //Check diagonals
+            state = CheckDiagonals(gameBoard, state);
+
+            //Check for cat's game only if there is no winner
+            if (state == GameState.NoWinner)
+            {
+                state = CheckCatsGame(gameBoard, state);
+            }
+
+            return state;
+        }
+
+        /// <summary>
+        /// Check rows for equality. Requires a gameboard and state.
+        /// </summary>
+        /// <param name="gameBoard"></param>
+        /// <param name="state"></param>
+        /// <returns></returns>
+        private static GameState CheckRows(char[][] gameBoard, GameState state)
+        {
             foreach (var line in gameBoard)
             {
                 char firstItem = line[0];
-                bool lineEqual = gameBoard[0]
+                bool lineEqual = line
                     .Skip(1)
                     .All(s => s.Equals(firstItem));
 
+                //If equal, determine winner
                 if (lineEqual)
                 {
-
+                    state = DetermineWinner(firstItem);
+                    break;
                 }
             }
+
+            return state;
         }
 
-        private char DetermineWinner(char winner)
+        /// <summary>
+        /// Checks columns for equality. Requires a gameboard and state.
+        /// </summary>
+        /// <param name="gameBoard"></param>
+        /// <param name="state"></param>
+        /// <returns></returns>
+        private static GameState CheckColumns(char[][] gameBoard, GameState state)
         {
-            
+            int length = gameBoard[0].Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                //Vertical array
+                char[] vertical = new char[length];
+
+                //Add to array
+                for (int j = 0; j < length; j++)
+                {
+                    vertical[j] = gameBoard[j][i];
+                }
+
+                //Check equality
+                char firstItem = vertical[0];
+                bool colEqual = vertical
+                    .Skip(1)
+                    .All(s => s.Equals(firstItem));
+
+                if (colEqual)
+                {
+                    state = DetermineWinner(firstItem);
+                    break;
+                }
+            }
+
+            return state;
+        }
+
+        /// <summary>
+        /// Checks diagonals for equality. Requires a gameboard and state.
+        /// </summary>
+        /// <param name="gameBoard"></param>
+        /// <param name="state"></param>
+        /// <returns></returns>
+        private static GameState CheckDiagonals(char[][] gameBoard, GameState state)
+        {
+            int length = gameBoard[0].Length;
+
+            char[] diagonal1 = new char[length];
+            char[] diagonal2 = new char[length];
+
+            //Get diagonals
+            for (int i = 0; i < length; i++)
+            {
+                diagonal1[i] = gameBoard[i][i];
+                diagonal2[i] = gameBoard[length - 1 - i][length - 1 - i];
+            }
+
+            //Check diagonal 1
+            char dChar1 = diagonal1[0];
+            bool diagOneEqual = diagonal1
+                .Skip(1)
+                .All(s => s.Equals(dChar1));
+
+            //Check diagonal 2
+            char dChar2 = diagonal2[0];
+            bool diagTwoEqual = diagonal2
+                .Skip(1)
+                .All(s => s.Equals(dChar2));
+
+            //Check for diagonal winners
+            if (diagOneEqual)
+            {
+                state = DetermineWinner(dChar1);
+            }
+            if (diagTwoEqual)
+            {
+                state = DetermineWinner(dChar2);
+            }
+
+            return state;
+        }
+
+        /// <summary>
+        /// Checks for a full board with no winner. Requires a gameboard and state.
+        /// </summary>
+        /// <param name="gameBoard"></param>
+        /// <param name="state"></param>
+        /// <returns></returns>
+        private static GameState CheckCatsGame(char[][] gameBoard, GameState state)
+        {
+            char[] playerArray = { 'X', 'O' };
+            bool ContainsNumbers = false;
+
+            foreach (var line in gameBoard)
+            {
+                if (line.Any(x => Char.IsDigit(x)))
+                {
+                    ContainsNumbers = true;
+                }
+            }
+
+            if (!ContainsNumbers)
+            {
+                state = GameState.Cat;
+            }
+
+            return state;
+        }
+
+        /// <summary>
+        /// Checks if line is equal. Currently incomplete. Probably won't finish.
+        /// </summary>
+        /// <param name="row"></param>
+        /// <returns></returns>
+        private static bool IsLineEqual(char[] row)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Determines which enum value to return based on char input.
+        /// </summary>
+        /// <param name="winner"></param>
+        /// <returns></returns>
+        private static GameState DetermineWinner(char winner)
+        {
+            switch(winner)
+            {
+                case 'X': return GameState.X;
+                case 'O': return GameState.O;
+                default: return GameState.NoWinner;
+            }
         }
     }
 }
--- Mission3/Program.cs
+++ Mission3/Program.cs
@@ -11,25 +11,12 @@ namespace Mission3
 
             char[][] gameBoard = new char[3][]; // Create game board
 
-            gameBoard[0] = new char[] { '1', '2', '3' };
-            gameBoard[1] = new char[] { '4', '5', '6' };
-            gameBoard[2] = new char[] { '7', '8', '9' };
+            gameBoard[0] = new char[] { 'X', 'O', 'X' };
+            gameBoard[1] = new char[] { 'O', 'X', 'O' };
+            gameBoard[2] = new char[] { 'O', 'X', 'O' };
 
-            var state = BoardInfo.CheckWin();
-
-            while (state == GameState.NoWinner)
-            {
-                Console.WriteLine("Please enter the number to place the 'X': ");
-
-
-                if (state == GameState.NoWinner)
-                {
-                    Console.WriteLine("Please enter the number to place the 'O': ");
-                }
-                
-            }
-            
-
+            BoardInfo.PrintBoard(gameBoard);
+            Console.WriteLine(BoardInfo.CheckWin(gameBoard));
         }
     }
 }
