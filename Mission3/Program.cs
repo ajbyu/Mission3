4fa1f2c71da1c33e233de4400b401f627c3a9384
nbain00 <zink47@gmail.com>
2023-01-24 16:51:46 -0700

    updating game board

--- Mission3/Program.cs
+++ Mission3/Program.cs
@@ -11,21 +11,71 @@ namespace Mission3
 
             char[][] gameBoard = new char[3][]; // Create game board
 
-            gameBoard[0] = new char[] { 'X', 'O', 'X' };
-            gameBoard[1] = new char[] { 'O', 'X', 'O' };
-            gameBoard[2] = new char[] { 'O', 'X', 'O' };
+            gameBoard[0] = new char[] { '1', '2', '3' };
+            gameBoard[1] = new char[] { '4', '5', '6' };
+            gameBoard[2] = new char[] { '7', '8', '9' };
 
             var state = BoardInfo.CheckWin(gameBoard);
 
             while (state == GameState.NoWinner)
             {
-                Console.WriteLine("Please enter the number to place the 'X': ");
+                bool validInput = false;
+                while (validInput == false)
+                {
+                    BoardInfo.PrintBoard(gameBoard);
+                    Console.WriteLine("Please enter the number to place the 'X': ");
+                    string input = Console.ReadLine();
+                    char newInput = char.Parse(input);
 
+                    for (int i = 0; i < gameBoard.Length; i++)
+                    {
+                        for (int j = 0; j < gameBoard[i].Length; j++)
+                        {
+                            if (gameBoard[i][j] == newInput)
+                            {
+                                validInput = true;
+                                gameBoard[i][j] = 'X';
+                                break;
+                            }
+                            else if (i == gameBoard.Length & j == gameBoard.Length & validInput == false)
+                            {
+                                Console.WriteLine("Please enter a valid input");
+                            }
+                        }
+                    }
+                }
 
+                state = BoardInfo.CheckWin(gameBoard);
                 if (state == GameState.NoWinner)
                 {
-                    Console.WriteLine("Please enter the number to place the 'O': ");
+                    validInput = false;
+                    while (validInput == false)
+                    {
+                        BoardInfo.PrintBoard(gameBoard);
+                        Console.WriteLine("Please enter the number to place the 'O': ");
+                        string input = Console.ReadLine();
+                        char newInput = char.Parse(input);
+
+                        for (int i = 0; i < gameBoard.Length; i++)
+                        {
+                            for (int j = 0; j < gameBoard[i].Length; j++)
+                            {
+                                if (gameBoard[i][j] == newInput)
+                                {
+                                    validInput = true;
+                                    gameBoard[i][j] = 'O';
+                                    break;
+                                }
+                                else if (i == gameBoard.Length & j == gameBoard.Length & validInput == false)
+                                {
+                                    Console.WriteLine("Please enter a valid input");
+                                }
+                            }
+                        }
+                    }
                 }
+
+                state = BoardInfo.CheckWin(gameBoard);
                 
             }
             
